1f04b935778b16a673558f24332e21be983861f4
Frankie Gavin <frankanthonygavin@gmail.com>
2016-10-28 15:16:47 -0700

    add proper indentation and spacing as well as divison in back end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // back-end logic
-function pingPongFun(userInput){
+function pingpongStart(userInput){
 var result = [];
 for (var i = 1; i <= userInput; i++){
   if ((i % 15) === 0){
@@ -19,7 +19,7 @@ $(document).ready(function() {
   $("form#ppForm").submit(function(event) {
     $("#result").empty();
     var userInput = parseInt($("input#enterNumber").val());
-    var result = pingPongFun(userInput);
+    var result = pingpongStart(userInput);
     result.forEach(function(output) {
     $("#result").append("<li>" + output + "</li>");
     event.preventDefault();
