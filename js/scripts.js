832d61efe838eb87d3d7c118601fc2b6edb5ad7e
Frankie Gavin <frankanthonygavin@gmail.com>
2016-10-28 13:59:33 -0700

    add completed counter and if else statements for cases of numbersdivisible by 3,5,15

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-$(document).ready(function() {
-  $("form#pingpongn").submit(function(event) {
-  event.preventDefault();
-  var enteredNumber = $("input#enterNumber").val();
-
-  var hereWeGo = "";
-
-  var numberList = (enteredNumber);
-for (var i = 1; i <= numberList; i++) {
-
-  return answers.empty();
+// back-end logic
+function pingPongFun(userInput){
+var result = [];
+for (var i = 1; i <= userInput; i++){
+  if ((i % 15) === 0){
+    result.push ("ping-pong");
+  } else if ((i % 5) === 0){
+    result.push ("pong");
+  } else if ((i % 3) === 0){
+    result.push ("ping");
+  } else
+    result.push (i);
 }
-    });
+return result;
+}
+
+// front-end logic
+$(document).ready(function() {
+  $("form#ppForm").submit(function(event) {
+    $("#result").empty();
+    var userInput = parseInt($("input#enterNumber").val());
+    var result = pingPongFun(userInput);
+    result.forEach(function(output) {
+    $("#result").append("<li>" + output + "</li>");
+    event.preventDefault();
   });
+});
+});
