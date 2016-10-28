205c96088bd94ea1a10be162095f0ef5ede5a111
Frankie Gavin <frankanthonygavin@gmail.com>
2016-10-28 12:00:39 -0700

    add non working number counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-$(document).ready(function(){
-  $("form#pingpong").submit(function(event) {
+$(document).ready(function() {
+  $("form#pingpongn").submit(function(event) {
+  event.preventDefault();
+  var enteredNumber = $("input#enterNumber").val();
 
-    var userInput = $("input#enterNumber").val()
-    event.preventDefault();
-  $("#resultDisplay").text(userInput);
+  var hereWeGo = "";
+
+  var numberList = (enteredNumber);
+for (var i = 1; i <= numberList; i++) {
+
+  return answers.empty();
+}
+    });
   });
-});
