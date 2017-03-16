443167e81d11280b4fd461ecbc5abd50be44f6d7
Frankie Gavin <frankanthonygavin@gmail.com>
2017-03-16 10:31:54 -0700

    change % to / to stop remainders from skipping numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 function pingStart(userInput){
 var result = [];
 for (var i = 1; i <= userInput; i++){
-  if ((i % 15) === 0){
+  if ((i / 15) === 0){
     result.push ("ping-pong");
-  } else if ((i % 5) === 0){
+  } else if ((i / 5) === 0){
     result.push ("pong");
-  } else if ((i % 3) === 0){
+  } else if ((i / 3) === 0){
     result.push ("ping");
   } else
     result.push (i);
